137784bcbe5d88ec6f605b0b84de282ae04fd4b6
Andrey Popov <andrej_1987@bk.ru>
2022-08-19 23:06:36 +0500

    Добавьте файлы проекта.

---
+++ Deligate001/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deligate001
+{
+    class Class1
+    {
+        static public void Print(string str)
+        {
+            
+            Console.WriteLine(str);
+        }
+        static public void Space(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.Write(str[i]);
+                if (i % 2 != 0)
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+        static public void countSym(string str)
+        {
+            Console.WriteLine("Колличество элементов в строке - {0} = {1}", str, str.Length);
+        }
+
+    }
+}
---
+++ Deligate001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Deligate001
+{
+    delegate void myDelegate(string myStr);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str1 = Console.ReadLine();
+            myDelegate print = Class1.Print;
+            print += Class1.Space;
+            print += Class1.countSym;
+            print(str1);
+            
+        }
+    }
+}
+
+
